d8376f01aa69b8f1db8ac05c7344d80c7ec9dacf
dabaoshu <478219876@qq.com>
2024-01-14 23:21:08 +0800

    编译

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,12 +1,8 @@
-import * as dayjs from 'dayjs'
-import * as isLeapYear from 'dayjs/plugin/isLeapYear' // 导入插件
-import * as duration from 'dayjs/plugin/duration' // 导入插件
-import 'dayjs/locale/zh-cn' // 导入本地化语言
+import  dayjs from "dayjs";
+import isLeapYear from "dayjs/plugin/isLeapYear"; // 导入插件
+import duration from "dayjs/plugin/duration"; // 导入插件
+import "dayjs/locale/zh-cn"; // 导入本地化语言
 
-dayjs.extend(isLeapYear) // 使用插件
-dayjs.extend(duration) // 使用插件
-dayjs.locale('zh-cn') // 使用本地化语言
-console.log(
-  
-dayjs(1697785858408).format("HH:mm:ss")
-);
+dayjs.extend(isLeapYear); // 使用插件
+dayjs.extend(duration); // 使用插件
+dayjs.locale("zh-cn"); // 使用本地化语言
